d53cadfc7e5383389b0d37433927498509326c1e
Raja Yaser Hussain <yaser@Yasers-MacBook-Pro.local>
2024-03-27 00:19:48 -0400

    Added code for connection.js and index.js, and added User and Thought models

---
+++ models/Thought.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectID(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: date => date.toLocaleDateString(),
+        },
+    }
+);
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: date => date.toLocaleDateString(),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    }
+);
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trimmed: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought',
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user',
+            }
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return `${this.friends.length}`;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
+
