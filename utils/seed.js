05a5befccc9a9ea5571ac8ac1073b1018922e834
Raja Yaser Hussain <yaser@Yasers-MacBook-Pro.local>
2024-03-28 17:30:25 -0400

    Finished code for seeding database

--- utils/seed.js
+++ utils/seed.js
@@ -1,6 +1,32 @@
 // Import connection
 const connection = require('../config/connection');
 // Import models
-const { User, Thought } = require('../models');
+const { User, Thought, Reaction } = require('../models');
 // Import data
-const data = require('./data');
+const { users, thoughts, reactions } = require('./data');
+// Open connection
+connection.once('open', async () => {
+    // Drop users collection if it already exists
+    let userCheck = await connection.db.listCollections({ name: 'users' }).toArray;
+    if (userCheck.length) {
+        await connection.dropCollection('users');
+    }
+    // Drop thoughts collection if it already exists
+    let thoughtCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray;
+    if (thoughtCheck.length) {
+        await connection.db.dropCollection('thoughts');
+    }
+    // Drop reactions collection if it already exists
+    let reactionCheck = await connection.db.listCollections({ name: 'reactions' }).toArray;
+    if (reactionCheck.length) {
+        await connection.db.dropCollection('reactions');
+    }
+    // Seed users, thoughts, and reactions
+    await User.collection.insertMany(users);
+    await Thought.collection.insertMany(thoughts);
+    await Reaction.collection.insertMany(reactions);
+    // Log seeding results
+    console.table(users);
+    console.table(thoughts);
+    console.table(reactions);
+});
