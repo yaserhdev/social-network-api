3cc9b82363de8432ec43a8f1ce953fcfaf18968f
Raja Yaser Hussain <yaser@Yasers-MacBook-Pro.local>
2024-03-28 00:48:22 -0400

    Started code for data and seed files

---
+++ utils/data.js
@@ -0,0 +1,117 @@
+const users = [
+    // {
+    //     username: , 
+    //     email: , 
+    //     thoughts: {
+    //         thoughtText: '', 
+    //         username: , 
+    //         reactions: {
+    //             reactionBody: '',
+    //             username: 
+    //         }
+    //     },
+    //     friends: []
+    // }
+    {
+        username: ,
+        email: ,
+        thoughts: [],
+        friends: []
+    },
+    {
+        username: ,
+        email: ,
+        thoughts: [],
+        friends: []
+    },
+    {
+        username: ,
+        email: ,
+        thoughts: [],
+        friends: []
+    }
+];
+
+const thoughts = [
+    {
+        thoughtText: '',
+        username: ,
+        reactions: []
+    },
+    {
+        thoughtText: '',
+        username: ,
+        reactions: []
+    },
+    {
+        thoughtText: '',
+        username: ,
+        reactions: []
+    },
+    {
+        thoughtText: '',
+        username: ,
+        reactions: []
+    },
+    {
+        thoughtText: '',
+        username: ,
+        reactions: []
+    },
+    {
+        thoughtText: '',
+        username: ,
+        reactions: []
+    }
+];
+
+const reactions = [
+    {
+        reactionBody: '',
+        username: 
+    },
+    {
+        reactionBody: '',
+        username: 
+    },
+    {
+        reactionBody: '',
+        username: 
+    },
+    {
+        reactionBody: '',
+        username: 
+    },
+    {
+        reactionBody: '',
+        username: 
+    },
+    {
+        reactionBody: '',
+        username: 
+    },
+    {
+        reactionBody: '',
+        username: 
+    },
+    {
+        reactionBody: '',
+        username: 
+    },
+    {
+        reactionBody: '',
+        username: 
+    },
+    {
+        reactionBody: '',
+        username: 
+    },
+    {
+        reactionBody: '',
+        username: 
+    },
+    {
+        reactionBody: '',
+        username: 
+    }
+];
---
+++ utils/seed.js
@@ -0,0 +1,6 @@
+// Import connection
+const connection = require('../config/connection');
+// Import models
+const { User, Thought } = require('../models');
+// Import data
+const data = require('./data');
